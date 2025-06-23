6979bb4974ca7cbb23bec9bcfd7e7d35c8625e36
CharlieB <charlie@superiordigital.design>
2025-06-23 18:00:44 -0300

    feat:patient creation and get all

--- src/patients/patients.module.ts
+++ src/patients/patients.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { PatientsService } from './patients.service';
 import { PatientsController } from './patients.controller';
+import { PatientRepository } from './repositories/patient.repository';
+import { PrismaPatientsRepository } from './repositories/prisma/prisma-repository';
+import { DbModule } from 'src/db/db.module';
 
 @Module({
+  imports: [DbModule],
   controllers: [PatientsController],
-  providers: [PatientsService],
+  providers: [
+    PatientsService,
+    {
+      provide: PatientRepository,
+      useClass: PrismaPatientsRepository,
+    },
+  ],
 })
 export class PatientsModule {}
--- src/patients/patients.service.ts
+++ src/patients/patients.service.ts
@@ -1,26 +1,53 @@
-import { Injectable } from '@nestjs/common';
-import { CreatePatientDto } from './dto/create-patient.dto';
-import { UpdatePatientDto } from './dto/update-patient.dto';
+import { HttpStatus, Injectable, Logger } from '@nestjs/common';
+import {
+  CreatePatientDto,
+  CreatePatientResponseDto,
+} from './dto/create-patient.dto';
+
+import { PatientRepository } from './repositories/patient.repository';
+import { Patient } from './domain/patient.domain';
+import { PatientException } from './exceptions/patient.exception';
+import { GetAllPatientsDto } from './dto/get-all-patients.dto';
 
 @Injectable()
 export class PatientsService {
-  create(createPatientDto: CreatePatientDto) {
-    return 'This action adds a new patient';
+  constructor(private readonly patientRepository: PatientRepository) {}
+  private readonly logger = new Logger(PatientsService.name);
+
+  async create(createPatientDto: CreatePatientDto) {
+    const patientExists = await this.patientRepository.findByEmail(
+      createPatientDto.email,
+    );
+    if (patientExists) {
+      this.logger.error(
+        `Patient with email ${createPatientDto.email} already exists.`,
+      );
+      throw new PatientException(
+        `Patient with email ${createPatientDto.email} already exists.`,
+        HttpStatus.CONFLICT,
+      );
+    }
+
+    const patient = Patient.create(createPatientDto);
+    try {
+      await this.patientRepository.create(patient);
+    } catch (error) {
+      this.logger.error('Error creating patient', error);
+      throw new PatientException(
+        'Error creating patient',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+
+    return new CreatePatientResponseDto(patient);
   }
 
-  findAll() {
-    return `This action returns all patients`;
+  findByEmail(email: string) {
+    return this.patientRepository.findByEmail(email);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} patient`;
-  }
-
-  update(id: number, updatePatientDto: UpdatePatientDto) {
-    return `This action updates a #${id} patient`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} patient`;
+  async findAll() {
+    const getAllPatients = await this.patientRepository.findAll();
+    return new GetAllPatientsDto(getAllPatients);
   }
 }
