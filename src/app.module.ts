210a15a38d03497ce7800358fd68b918132813db
CharlieB <charlie@superiordigital.design>
2025-06-23 15:54:17 -0300

    feat:connect db to back end

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PatientsModule } from './patients/patients.module';
+import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [PatientsModule],
+  imports: [PatientsModule, DbModule],
   controllers: [AppController],
   providers: [AppService],
 })
