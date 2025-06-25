90820633d09eb54b435cda0e3521e138f3e65ce7
CharlieB <charlie@superiordigital.design>
2025-06-25 09:17:25 -0300

    add future sms implementation create notification module to handle multiple types of notifications sms or email

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PatientsModule } from './patients/patients.module';
 import { DbModule } from './db/db.module';
 import { MailModule } from './mail/mail.module';
+import { NotificationModule } from './notification/notification.module';
 
 @Module({
-  imports: [PatientsModule, DbModule, MailModule],
+  imports: [PatientsModule, DbModule, MailModule, NotificationModule],
   controllers: [AppController],
   providers: [AppService],
 })
